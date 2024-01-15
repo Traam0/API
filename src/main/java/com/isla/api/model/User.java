02b6073e38f5bf99cb5d7080a849e7fdad567b92
traam0 <younesmrx@outlook.com>
2024-01-15 19:30:56 +0100

    [updated] User model persists with db

--- src/main/java/com/isla/api/model/User.java
+++ src/main/java/com/isla/api/model/User.java
@@ -2,11 +2,128 @@ package com.isla.api.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
-public record User(Integer id, @NotBlank String username, @Email String email, @NotBlank String password,
-        LocalDateTime createdAt, LocalDateTime updatedAt,
-        @NotBlank Role role,
-        @NotBlank Status state) {
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Column(nullable = true)
+    private LocalDateTime email_verified;
+    private LocalDateTime createdAt;
+    @Column(nullable = true)
+    private LocalDateTime updatedAt;
+    private String version;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(Long id, String username, String email, String password, LocalDateTime email_verified,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt, String version, Role role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.email_verified = email_verified;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.version = version;
+        this.role = role;
+    }
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getEmail_verified() {
+        return email_verified;
+    }
+
+    public void setEmail_verified(LocalDateTime email_verified) {
+        this.email_verified = email_verified;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
 }
